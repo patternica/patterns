0910c986f801f545698c289fe539b5a61c7ab349
Anton007 <posttkachov@gmail.com>
2019-12-10 11:00:40 +0200

    Singleton added

---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public final class Singleton {
+    private static final Singleton instance = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
